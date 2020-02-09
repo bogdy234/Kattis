3f69769f443d454b7bf08478681fe6e8f74e6b41
bogdy234 <53002105+bogdy234@users.noreply.github.com>
2020-02-09 21:46:02 +0200

    Add files via upload
    Autori Kattis

--- main.c
+++ main.c
@@ -1,21 +1,39 @@
-// A different problem Kattis
+/*
+Autori
+Great scientific discoveries are often named by the last names of scientists that made them. For example, the most popular asymmetric cryptography system, RSA was discovered by Rivest, Shamir and Adleman. Another notable example is the Knuth-Morris-Pratt algorithm, named by Knuth, Morris and Pratt.
 
+Scientific papers reference earlier works a lot and it’s not uncommon for one document to use two different naming conventions: the short variation (e.g. KMP) using only the first letters of authors last names and the long variation (e.g. Knuth-Morris-Pratt) using complete last names separated by hyphens.
+
+We find mixing two conventions in one paper to be aesthetically unpleasing and would like you to write a program that will transform long variations into short.
+
+Input
+The first and only line of input will contain at most 100 characters, uppercase and lowercase letters of the English alphabet and hyphen (‘-’ ASCII 45). The first character will always be an uppercase letter. Hyphens will always be followed by an uppercase letter. All other characters will be lowercase letters.
+
+Output
+The first and only line of output should contain the appropriate short variation.
+
+Sample Input 1	Sample Output 1
+Knuth-Morris-Pratt
+KMP
+Sample Input 2	Sample Output 2
+Mirko-Slavko
+MS
+Sample Input 3	Sample Output 3
+Pasko-Patak
+PP
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-long long int absolut(long long int x)
-{
-    if (x<0)
-        x=-x;
-    return x;
-}
+#include <string.h>
 
 int main()
 {
-    long long int x=0,y;
-    char z;
-      while(scanf("%lli",&x)&& scanf("%lli",&y))
-        printf("%lli\n",absolut(x-y));
+    char a[100];
+    int i;
+    scanf("%s",&a);
+    for (i=0;i<strlen(a);i++)
+        if (i==0 || a[i-1]=='-')
+          printf("%c",a[i]);
     return 0;
 }

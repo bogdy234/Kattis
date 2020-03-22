7a6859742059fe67e63723f13137ae4318b3021c
bogdy234 <53002105+bogdy234@users.noreply.github.com>
2020-03-23 00:21:44 +0200

    Add files via upload

---
+++ Kattis/A Real Challange Kattis/Main.java
@@ -0,0 +1,15 @@
+package A.Real.Challange;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long a;
+        double x,rez;
+        a=sc.nextLong();
+        x= Math.sqrt(a);
+        rez=4*x;
+        System.out.println(rez);
+    }
+}
